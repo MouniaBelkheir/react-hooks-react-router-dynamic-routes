2902ec17112db9b86dfe8b67785f79fbd37dc1a1
Mounia Belkheir <mouniabelkheir@outlook.com>
2023-10-09 14:51:19 +0100

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,17 @@
-import React, { useState } from "react";
-import { Route, Switch } from "react-router-dom";
-import NavBar from "./NavBar";
-import MoviesPage from "./MoviesPage";
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import MoviesPage from './MoviesPage';
 
 function App() {
-  const [movies, setMovies] = useState([
-    { id: 1, title: "A River Runs Through It" },
-    { id: 2, title: "Se7en" },
-    { id: 3, title: "Inception" }
-  ]);
+  const [movies, setMovies] = useState({
+    1: { id: 1, title: "A River Runs Through It" },
+    2: { id: 2, title: "Se7en" },
+    3: { id: 3, title: "Inception" },
+  });
 
   return (
-    <div>
-      <NavBar />
+    <Router>
       <Switch>
         <Route path="/movies">
           <MoviesPage movies={movies} />
@@ -21,8 +20,9 @@ function App() {
           <div>Home</div>
         </Route>
       </Switch>
-    </div>
+    </Router>
   );
 }
 
 export default App;
+
---
+++ src/components/MovieShow.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+
+function MovieShow({ movies }) {
+  const params = useParams();
+  const selectedMovie = movies[params.movieId];
+
+  if (!selectedMovie) {
+    return <div>Movie not found</div>;
+  }
+
+  return (
+    <div>
+      <h3>{selectedMovie.title}</h3>
+      {/* Display other movie details as needed */}
+    </div>
+  );
+}
+
+export default MovieShow;
